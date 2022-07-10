83b78a289bbe7306f71fce106f459ae52ed07e04
Nabil Abu-Hadba <nabilabuhadba@gmail.com>
2022-07-10 17:28:07 +0300

    Finished pullin trending coins data from coingecko api and styling it on homepage

--- cryptowatch/src/routes/CoinPage.jsx
+++ cryptowatch/src/routes/CoinPage.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const CoinPage = () => {
+    const [coin, setCoin] = useState({})
+    const url = 'https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true'
+
     return (
         <div>CoinPage</div>
     )
