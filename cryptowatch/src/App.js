35d7bd1679f3b470b2059b5a6a34b70f521fb0c3
Nabil Abu-Hadba <nabilabuhadba@gmail.com>
2022-07-10 21:09:42 +0300

    finished the footer styles and functionality

--- cryptowatch/src/App.js
+++ cryptowatch/src/App.js
@@ -8,6 +8,7 @@ import Signup from './routes/Signup'
 import Account from './routes/Account'
 import CoinPage from './routes/CoinPage'
 import axios from 'axios'
+import Footer from './components/Footer';
 
 function App() {
   const [coins, setCoins] = useState([])
@@ -32,6 +33,7 @@ function App() {
           <Route path=':coinId' />
         </Route>
       </Routes>
+      <Footer />
     </ThemeProvider>
   );
 }
