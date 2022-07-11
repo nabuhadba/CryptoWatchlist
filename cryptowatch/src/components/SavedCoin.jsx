09c0b2b625705b735f2e40f73204d31ecd625457
Nabil Abu-Hadba <nabilabuhadba@gmail.com>
2022-07-12 00:26:22 +0300

    completed sign in and sign up with firebase and adding coins to watchlist on the backend only not forntend yet.

--- cryptowatch/src/components/SavedCoin.jsx
+++ cryptowatch/src/components/SavedCoin.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore'
+import { db } from '../firebase'
+import { UserAuth } from '../context/AuthContext'
 
 const SavedCoin = () => {
     const [coins, setCoins] = useState([])
+    const { user } = UserAuth()
+
+
 
     return (
         <div>
